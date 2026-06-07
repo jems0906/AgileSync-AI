4ba91bd9aa09e461040c7305e73550dfa7a73122
jems0906 <dorviene09@gmail.com>
2026-06-07 14:54:56 -0400

    Fix CHROME_PATH propagation for pa11y

--- .pa11yci.cjs
+++ .pa11yci.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   defaults: {
     chromeLaunchConfig: {
-      executablePath: process.env.CHROME_PATH,
+      executablePath: process.env.CHROME_PATH || process.env.PUPPETEER_EXECUTABLE_PATH,
       args: ['--no-sandbox']
     }
   },
